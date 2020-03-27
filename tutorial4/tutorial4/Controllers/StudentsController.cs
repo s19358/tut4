727778f5e8709a4ba181ce6d4e15428de7e93a3f
s19358 <s19358@pjwstk.edu.pl>
2020-03-27 13:59:01 +0100

    initial commit

---
+++ tutorial4/tutorial4/Controllers/StudentsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using tutorial4.Models;
+
+namespace tutorial4.Controllers
+{
+
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+
+        private string ConnString = "Data Source=db-mssql;Initial Catalog=s19358;Integrated Security=True";
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+
+
+            var listStudents = new List<Student>();
+            using (SqlConnection con = new SqlConnection(ConnString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from Student";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var student = new Student();
+
+                    student.IndexNumber = dr["IndexNumber"].ToString();
+                    student.FirstName = dr["FirstName"].ToString();
+                    student.LastName = dr["LastName"].ToString();
+                    student.BirthDate = dr["BirthDate"].ToString();
+                    student.idEnrollment = dr["idEnrollment"].ToString();
+
+
+
+
+                    listStudents.Add(student);
+                }
+
+                return Ok(listStudents);
+            }
+        }
+    }
+}
