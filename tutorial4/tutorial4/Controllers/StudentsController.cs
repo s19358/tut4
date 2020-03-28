b716db7b15b1bc95e2e8eac13c4c279222771e72
s19358 <s19358@pjwstk.edu.pl>
2020-03-28 22:22:57 +0100

    completed

--- tutorial4/tutorial4/Controllers/StudentsController.cs
+++ tutorial4/tutorial4/Controllers/StudentsController.cs
@@ -22,32 +22,115 @@ namespace tutorial4.Controllers
 
 
             var listStudents = new List<Student>();
+            using (SqlConnection con = new SqlConnection(ConnString))  // connect to the SQL Server database (opening,closing,managing the database)
+            using (SqlCommand com = new SqlCommand())  //represents sqlquery or other command to send the databse 
+            {                                              //using block otomatik olarak disposesu calistiriyor
+                com.Connection = con;
+                com.CommandText = "select FirstName, LastName, BirthDate, s.Name 'studies',e.Semester from Student stu " +
+                    "Join Enrollment e on stu.idEnrollment = e.idEnrollment " +
+                    "Join Studies s on s.idStudy = e.idStudy; ";
+
+                con.Open();  //opens connection to database it should open before sending command
+                SqlDataReader dr = com.ExecuteReader();  //executing command 
+                //sqldatareader -> fetch stream of data/rows
+                while (dr.Read())  //fetch next record
+                {
+                    var student = new Student();
+
+                    student.FirstName = dr["FirstName"].ToString();  //name of the column
+                    student.LastName = dr["LastName"].ToString();
+                    student.BirthDate = dr["BirthDate"].ToString();
+                    student.studies = dr["studies"].ToString();
+                    student.semester = dr.GetInt32(4);
+                    //index of the column but it could be a problem because order of columns can change
+
+                    listStudents.Add(student);
+                }
+                //con.Dispose();
+
+                return Ok(listStudents);
+            }
+
+
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSemesterEntry(string id)
+        {
             using (SqlConnection con = new SqlConnection(ConnString))
             using (SqlCommand com = new SqlCommand())
             {
                 com.Connection = con;
-                com.CommandText = "select * from Student";
+                // com.CommandText = "select e.StartDate from Student s Join Enrollment e on s.idEnrollment = e.idEnrollment where s.IndexNumber='"+id+"';" ;
+                //this is unsafe we should protect the databases from sqlinjection attacks so
+                com.CommandText = "select e.StartDate from Student s " +
+                    "Join Enrollment e on s.idEnrollment = e.idEnrollment where s.IndexNumber=@index";
+
+                com.Parameters.AddWithValue("index", id);
+                string date;
+                con.Open();
+                var dr = com.ExecuteReader();
+                if (dr.Read()) //if it exists
+                {
+                    date = dr["StartDate"].ToString();
+                    return Ok(date);
+                }
+                return Ok();
+
+
+            }
+        }
+
+        //adding new data to the database
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            using (SqlConnection con = new SqlConnection(ConnString))
+            using (SqlCommand com = new SqlCommand())
+            {
+
+                var index = $"s{new Random().Next(1, 20000)}";
+                com.Connection = con;
+                com.CommandText = "insert into Student values('" + index + "',@par1,@par2,@par3,2);";
+                com.Parameters.AddWithValue("par1", student.FirstName);
+                com.Parameters.AddWithValue("par2", student.LastName);
+                com.Parameters.AddWithValue("par3", student.BirthDate);
+                con.Open();
+                int affected = com.ExecuteNonQuery();
+                return Ok(affected);
+            }
+            return Ok();
+
+
+        }
+
+
+        [HttpGet("procedure")]
+        public IActionResult getStudentwithProc()
+        {
+            using (SqlConnection con = new SqlConnection(ConnString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "TestProc";
+                com.CommandType = System.Data.CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("FirstName", "Berkay");
 
                 con.Open();
-                SqlDataReader dr = com.ExecuteReader();
-                while (dr.Read())
+                var dr = com.ExecuteReader();
+                if (dr.Read())
                 {
                     var student = new Student();
 
-                    student.IndexNumber = dr["IndexNumber"].ToString();
                     student.FirstName = dr["FirstName"].ToString();
                     student.LastName = dr["LastName"].ToString();
                     student.BirthDate = dr["BirthDate"].ToString();
-                    student.idEnrollment = dr["idEnrollment"].ToString();
-
-
-
-
-                    listStudents.Add(student);
+                    return Ok(student);
                 }
 
-                return Ok(listStudents);
             }
+            return Ok();
         }
     }
 }
--- tutorial4/tutorial4/Models/Student.cs
+++ tutorial4/tutorial4/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,12 @@ namespace tutorial4.Models
 {
     public class Student
     {
-        public string IndexNumber { get; set; }
+        [Required(ErrorMessage ="you should enter a name!")] //checking the validation kullanici null yada bos biraktiysa vs diye
+        [MaxLength(20)]  //post calismadan once apicontoller validationi check etcek hata varsa o method calismicak
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string BirthDate { get; set; }
-        public string idEnrollment { get; set; }
+        public string studies { get; set; }
+        public int semester { get; set; }
     }
 }
